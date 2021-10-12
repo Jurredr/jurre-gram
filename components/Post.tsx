42d77baec543c49d80d3dcf871ecf53626f1f651
Jurredr <github@jurre.me>
2021-10-12 23:39:40 +0200

    Finished Post styling and base

--- components/Post.tsx
+++ components/Post.tsx
@@ -1,4 +1,13 @@
 import React from 'react'
+import {
+  BookmarkIcon,
+  ChatIcon,
+  DotsHorizontalIcon,
+  EmojiHappyIcon,
+  HeartIcon,
+  PaperAirplaneIcon
+} from '@heroicons/react/outline'
+import { HeartIcon as HeartIconFilled } from '@heroicons/react/solid'
 
 interface Props {
   key: number
@@ -11,8 +20,78 @@ interface Props {
 
 const Post: React.FC<Props> = (props) => {
   return (
-    <div>
-      <h1>I'm a post</h1>
+    <div className="bg-white my-7 border rounded-sm">
+      {/* Header */}
+      <div className="flex items-center p-4">
+        <img
+          className="rounded-full h-11 w-11 object-contain p-1 mr-3 cursor-pointer"
+          src={props.userImg}
+          alt="Avatar"
+        />
+        <div className="flex flex-1">
+          <p className="align-start font-semibold text-sm hover:underline cursor-pointer w-max-12">
+            {props.username}
+          </p>
+        </div>
+        <DotsHorizontalIcon className="h-5 cursor-pointer" />
+      </div>
+
+      {/* Image */}
+      <img className="object-cover w-full" src={props.img} alt="Post Image" />
+
+      {/* Buttons */}
+      <div className="flex justify-between px-4 pt-4">
+        <div className="flex gap-4">
+          <img
+            className="btn"
+            src="/heart-icon.svg"
+            alt="Like"
+            draggable="false"
+          />
+          <img
+            className="btn"
+            src="/comment-icon.svg"
+            alt="Comment"
+            draggable="false"
+          />
+          <img
+            className="btn"
+            src="/direct-icon.svg"
+            alt="Send"
+            draggable="false"
+          />
+        </div>
+        <img
+          className="btn"
+          src="/bookmark-icon.svg"
+          alt="Save"
+          draggable="false"
+        />
+      </div>
+
+      {/* Caption */}
+      <p className="p-5 truncate">
+        <span className="font-semibold mr-1">{props.username}</span>
+        {props.caption}
+      </p>
+
+      {/* Comments */}
+
+      {/* Comment Input */}
+      <form className="flex items-center px-4 py-2 border-0 border-t-[1px] border-t-gray-100">
+        <img
+          className="btn"
+          src="/emoji-icon.svg"
+          alt="Emoji"
+          draggable="false"
+        />
+        <input
+          type="text"
+          placeholder="Add a comment..."
+          className="border-none flex-1 focus:ring-0 outline-none"
+        />
+        <button className="font-semibold text-blue-400">Post</button>
+      </form>
     </div>
   )
 }
