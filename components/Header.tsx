15d613eb5aa07a576341755748635bb4be700b08
Jurredr <github@jurre.me>
2021-10-12 18:26:54 +0200

    Added base of Header component

---
+++ components/Header.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Image from 'next/image'
+
+const Header: React.FC = () => {
+  return (
+    <div>
+      {/* Left */}
+      <div>
+        <Image src="" layout="fill" />
+      </div>
+
+      {/* Middle */}
+
+      {/* Right */}
+    </div>
+  )
+}
+
+export default Header
