4934fad23e06cb08db49baf30091d3aa50455294
Jurredr <github@jurre.me>
2021-10-12 21:44:52 +0200

    Added Feed + Stories components

---
+++ components/Feed.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Stories from './Stories'
+
+const Feed: React.FC = () => {
+  return (
+    <div>
+      {/* Left */}
+      <section>
+        {/* Stories */}
+        <Stories />
+
+        {/* Posts */}
+      </section>
+
+      {/* Right */}
+      <section>
+        {/* Mini Profile */}
+        {/* Suggestions */}
+        {/* Footer */}
+      </section>
+    </div>
+  )
+}
+
+export default Feed
---
+++ components/Stories.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Stories: React.FC = () => {
+  return (
+    <div>
+      {}
+      {}
+    </div>
+  )
+}
+
+export default Stories
