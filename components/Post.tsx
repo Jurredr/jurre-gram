71557c7663f67266cbbad970b7f501cb10ebbc69
Jurredr <github@jurre.me>
2021-10-13 13:30:02 +0200

    Added commenting logic

--- components/Post.tsx
+++ components/Post.tsx
@@ -1,5 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { DotsHorizontalIcon } from '@heroicons/react/outline'
+import { addDoc, collection, serverTimestamp } from '@firebase/firestore'
+import { db } from '../firebase'
+import { useSession } from 'next-auth/react'
 
 interface Props {
   key: string
@@ -11,6 +14,27 @@ interface Props {
 }
 
 const Post: React.FC<Props> = (props) => {
+  const { data: session } = useSession()
+  const [commentInput, setCommentInput] = useState('')
+  const [comments, setComments] = useState([])
+
+  const sendComment = async (e: any) => {
+    e.preventDefault()
+
+    // Get comment, reset current
+    const commentToSend = commentInput
+    setCommentInput('')
+
+    await addDoc(collection(db, 'posts', props.id, 'comments'), {
+      comment: commentToSend,
+      // @ts-ignore
+      username: session.user.username,
+      // @ts-ignore
+      userImage: session.user.image,
+      timeStamp: serverTimestamp()
+    })
+  }
+
   return (
     <div className="bg-white my-7 border rounded-sm">
       {/* Header */}
@@ -85,10 +109,19 @@ const Post: React.FC<Props> = (props) => {
         />
         <input
           type="text"
+          value={commentInput}
+          onChange={(e) => setCommentInput(e.target.value)}
           placeholder="Add a comment..."
           className="border-none flex-1 focus:ring-0 outline-none"
         />
-        <button className="font-semibold text-blue-400">Post</button>
+        <button
+          type="submit"
+          disabled={!commentInput.trim()}
+          onClick={sendComment}
+          className="font-semibold text-blue-400"
+        >
+          Post
+        </button>
       </form>
     </div>
   )
